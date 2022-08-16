a4e9d075833c76ae58222f5b6071e27196017981
Shruti Shastri <shrutishastri49@gmail.com>
2022-08-16 18:24:13 +0530

    created repo

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+		<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name="Home" component={HomeScreen} />
+				</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
